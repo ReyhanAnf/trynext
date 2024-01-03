eda8de8f01a29b9b99e0dfc8fb6c0ea61658e90e
Reyhan Anderea Firdaus <reihan676590@gmail.com>
2024-01-03 15:56:28 +0700

    initial commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,8 @@
+import { Poppins, Inter } from 'next/font/google';
+ 
+export const inter = Inter({ subsets: ['latin'] });
+
+export const poppins = Poppins({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+});
